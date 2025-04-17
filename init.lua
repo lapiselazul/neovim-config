fe6c7fd5551e6f56cdb3f6b54dfe60ba71d2ea78
Elazul <lapiselazul@proton.me>
2025-04-17 21:03:51 +0300

    chore: initial github commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+
+local set = vim.opt
+
+set.shiftwidth = 2
+set.tabstop = 2
+set.softtabstop = 0
+set.expandtab = true
+
+set.number = true
+set.relativenumber = true
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+local M = {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+  	vim.cmd[[colorscheme tokyonight-night]]
+  end
+}
+
+return { M }
