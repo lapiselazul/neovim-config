b0f7702c511a8a1a5ff4a774b6bff816522d027c
Lapis Elazul <lapiselazul@proton.me>
2025-05-23 10:48:37 +0300

    refactor: file splitting

---
+++ lua/elazul/opts.lua
@@ -0,0 +1,29 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- To make the switch smoother (and for resizing splits)
+vim.o.mouse = 'a'
+ 
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+ 
+vim.opt.incsearch = true
+ 
+vim.opt.termguicolors = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.inccommand = "split"
+
+vim.opt.scrolloff = 10
+
+-- asks to save file when I try to quit with unsaved changes
+vim.opt.confirm = true
+
+vim.opt.updatetime = 450
+
+vim.opt.colorcolumn = "100"
